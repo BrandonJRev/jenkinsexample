719a21aec1b026b5503729fab25c604856bb79ad
Brandon J <jackson.b2029@gmail.com>
2020-02-07 11:06:13 -0500

    commit to repo

---
+++ add2.py
@@ -0,0 +1,10 @@
+#Jenkins example continued
+
+def jenkinsfun(int)-> int:
+    ''' Returns an integer int +2
+    Arguments:
+      int -- any integer
+    Returns:
+      int -- int + 2
+      '''
+    return int + 2
---
+++ tests/test_add2.py
@@ -0,0 +1,11 @@
+# Jenkins
+import add2
+
+def test_jenkinsfun():
+    '''     Returns an integer int +2
+    Arguments:
+      int -- any integer
+    Returns:
+      int -- int + 2
+      '''
+    assert add2.jenkinsfun(2) == 4
